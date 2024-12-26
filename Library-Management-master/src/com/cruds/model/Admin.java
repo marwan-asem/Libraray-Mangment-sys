aae5957fd1f19e86158b6c969df2c4d2c909bee4
Marwan Asem Ghanem <96083104+marwan-asem@users.noreply.github.com>
2024-12-26 13:04:04 +0200

    Update Admin.java

--- Library-Management-master/src/com/cruds/model/Admin.java
+++ Library-Management-master/src/com/cruds/model/Admin.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.cruds.model;
 
-/**
- *
- * @author Valar Morghulis
- */
+package com.cruds.model;
+/
 public class Admin {
     String id;
     String password;
